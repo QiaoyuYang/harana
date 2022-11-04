024f9995905c8c9b3c195a8923e7d09c3bc9e480
QiaoyuYang <qiaoyu.kelvin.yang@outlook.com>
2022-11-04 10:40:34 -0400

    Made some quick tests in program entry points

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 # The entry point of the training code
 
 from harana.train import main
+from harana.utils import chord
 
+import torch
+import torch.nn as nn
 import sys
+torch.manual_seed(0)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
