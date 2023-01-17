d6b12eac218c40ae6dd59fa980c2b64cb3c6054d
QiaoyuYang <qiaoyu.kelvin.yang@outlook.com>
2023-01-17 12:50:32 -0500

    disable transition probabilities

--- harana/models/semi_crf.py
+++ harana/models/semi_crf.py
@@ -1,30 +1,29 @@
 # my import
-from ..utils import core
-from ..utils import harmony
+from .. import tools
 
 # regular import
 import numpy as np
 import torch
 from torch import nn
 
+# The transition probability is disabled for now before a better formulation of the transition is proposed
 
 class SemiCRF(nn.Module):
 
-    def __init__(self, batch_size, sample_size, num_label, segment_max_len, device, transition_importance):
+    def __init__(self, batch_size, num_label, segment_max_len, transition_importance, device):
         super().__init__()
         self.batch_size = batch_size
-        self.sample_size = sample_size
         self.num_label = num_label
+        self.sample_size = tools.FRAMES_PER_SAMPLE
         self.segment_max_len = segment_max_len
+        self.transition_importance = transition_importance
         
         self.device = device
-
-        self.transition_importance = transition_importance
 
         self.segment_score = torch.zeros(self.batch_size, self.sample_size, self.sample_size, self.num_label)
         self.segment_score = self.segment_score.to(self.device)
 
-        # the last number in the chord label index list is BOS tag.
+        # the last number in the harmony label index list is BOS tag.
         #self.BOS_TAG_ID = num_label
 
         self.transitions = nn.Parameter(torch.empty(self.num_label, self.num_label))
@@ -35,36 +34,38 @@ class SemiCRF(nn.Module):
         nn.init.uniform_(self.transitions, 0, 1)
 
 
-    def compute_path_score(self, chord_seq):
+    def compute_path_score(self, harmony_index_seq):
         path_score = torch.zeros(self.batch_size)
         path_score = path_score.to(self.device)
 
 
         for batch_idx in range(self.batch_size):
-            label_idx_cur = int(chord_seq[batch_idx, 0].item())
+            label_idx_cur = int(harmony_index_seq[batch_idx, 0].item())
             label_idx_new = label_idx_cur
             
             start_frame = 0
             end_frame = 0
             transition_score_cur = 0
             for frame_idx in range(self.sample_size):
-                if chord_seq[batch_idx, frame_idx] != label_idx_cur:
+                if harmony_index_seq[batch_idx, frame_idx] != label_idx_cur:
                     end_frame = frame_idx - 1
-                    label_idx_new = int(chord_seq[batch_idx, frame_idx].item())
-                    transition_score_cur += self.transitions[label_idx_cur, label_idx_cur] * (end_frame - start_frame)
-                    transition_score_cur += self.transitions[label_idx_cur, label_idx_new]
-                    transition_score_cur *= self.transition_importance
+                    label_idx_new = int(harmony_index_seq[batch_idx, frame_idx].item())
+                    #transition_score_cur += self.transitions[label_idx_cur, label_idx_cur] * (end_frame - start_frame) + self.transitions[label_idx_cur, label_idx_new]
+                    #transition_score_cur += self.transitions[label_idx_cur, label_idx_new]
+                    #transition_score_cur *= self.transition_importance
                     segment_score_cur = self.segment_score[batch_idx, start_frame, end_frame, label_idx_cur] * (end_frame - start_frame + 1)
-                    path_score[batch_idx] += transition_score_cur + segment_score_cur
+                    #path_score[batch_idx] += transition_score_cur + segment_score_cur
+                    path_score[batch_idx] += + segment_score_cur
                     transition_score_cur = 0
                     start_frame = frame_idx
                     label_idx_cur = label_idx_new
             
             end_frame = frame_idx
-            transition_score_cur += self.transitions[label_idx_cur, label_idx_cur]
-            transition_score_cur *= self.transition_importance
+            #transition_score_cur += self.transitions[label_idx_cur, label_idx_cur]
+            #transition_score_cur *= self.transition_importance
             segment_score_cur = self.segment_score[batch_idx, start_frame, end_frame, label_idx_cur] * (end_frame - start_frame + 1)
-            path_score[batch_idx] = transition_score_cur + segment_score_cur
+            #path_score[batch_idx] = transition_score_cur + segment_score_cur
+            path_score[batch_idx] += segment_score_cur
         
         return path_score
 
@@ -84,23 +85,25 @@ class SemiCRF(nn.Module):
                 if end_frame + 1 <= self.segment_max_len:
                     segment_len = end_frame + 1
                     segment_score_cur_starting_at_0 = self.segment_score[:, 0, end_frame, label_cur_idx] * segment_len
-                    transition_score_cur_starting_at_0 = self.transitions[label_cur_idx, label_cur_idx] * (segment_len - 1) 
-                    transition_score_cur_starting_at_0 *= self.transition_importance
-                    complete_score_cur_starting_at_0 = segment_score_cur_starting_at_0.unsqueeze(-1) + transition_score_cur_starting_at_0.unsqueeze(-1)[None, :]
+                    #transition_score_cur_starting_at_0 = self.transitions[label_cur_idx, label_cur_idx] * (segment_len - 1) 
+                    #transition_score_cur_starting_at_0 *= self.transition_importance
+                    #complete_score_cur_starting_at_0 = segment_score_cur_starting_at_0.unsqueeze(-1) + transition_score_cur_starting_at_0.unsqueeze(-1)[None, :]
+                    complete_score_cur_starting_at_0 = segment_score_cur_starting_at_0.unsqueeze(-1)
                 
                 # Segments not starting at 0
                 # j + 1 is the length of the frame
-                segment_score_cur_with_chord_change = torch.stack([self.segment_score[:, end_frame - max_span + 2 + j, end_frame, label_cur_idx] * (j + 1) for j in range(max_span - 1)], dim=1)
-                transition_score_cur_with_chord_change = self.transitions[torch.tensor(range(self.num_label)), label_cur_idx] * self.transition_importance
-                transition_score_cur_with_chord_change = torch.stack([transition_score_cur_with_chord_change + self.transitions[label_cur_idx, label_cur_idx] * j for j in range(max_span - 1)], dim=0)
-                complete_score_cur_with_chord_change = segment_score_cur_with_chord_change[:, :, None] + transition_score_cur_with_chord_change[None, :, :]
+                segment_score_cur_with_harmony_change = torch.stack([self.segment_score[:, end_frame - max_span + 2 + j, end_frame, label_cur_idx] * (j + 1) for j in range(max_span - 1)], dim=1)
+                #transition_score_cur_with_harmony_change = self.transitions[torch.tensor(range(self.num_label)), label_cur_idx] * self.transition_importance
+                #transition_score_cur_with_harmony_change = torch.stack([transition_score_cur_with_harmony_change + self.transitions[label_cur_idx, label_cur_idx] * self.transition_importance * j for j in range(max_span - 1)], dim=0)
+                #complete_score_cur_with_harmony_change = segment_score_cur_with_harmony_change[:, :, None] + transition_score_cur_with_harmony_change[None, :, :]
+                complete_score_cur_with_harmony_change = segment_score_cur_with_harmony_change[:, :, None]
                 
-                log_alpha_pre_with_chord_change = log_alpha[:, end_frame - max_span + 1 : end_frame, :]
+                log_alpha_pre_with_harmony_change = log_alpha[:, end_frame - max_span + 1 : end_frame, :]
 
                 if end_frame + 1 <= self.segment_max_len:
-                    log_alpha[:, end_frame, label_cur_idx] = torch.cat([complete_score_cur_starting_at_0, (log_alpha_pre_with_chord_change + complete_score_cur_with_chord_change).flatten(start_dim=1)], dim=1).logsumexp(dim=1)
+                    log_alpha[:, end_frame, label_cur_idx] = torch.cat([complete_score_cur_starting_at_0, (log_alpha_pre_with_harmony_change + complete_score_cur_with_harmony_change).flatten(start_dim=1)], dim=1).logsumexp(dim=1)
                 else:
-                    log_alpha[:, end_frame, label_cur_idx] = (log_alpha_pre_with_chord_change + complete_score_cur_with_chord_change).flatten(start_dim=1).logsumexp(dim=1)
+                    log_alpha[:, end_frame, label_cur_idx] = (log_alpha_pre_with_harmony_change + complete_score_cur_with_harmony_change).flatten(start_dim=1).logsumexp(dim=1)
         
         log_z = log_alpha[:, self.sample_size - 1, :].logsumexp(dim=1)
 
@@ -126,29 +129,31 @@ class SemiCRF(nn.Module):
                     if end_frame + 1 <= self.segment_max_len:
                         segment_len = end_frame + 1
                         segment_score_cur_starting_at_0 = self.segment_score[sample_idx, 0, end_frame, label_cur_idx] * segment_len
-                        transition_score_cur_starting_at_0 = self.transitions[label_cur_idx, label_cur_idx] * (segment_len - 1) 
-                        transition_score_cur_starting_at_0 *= self.transition_importance
-                        complete_score_cur_starting_at_0 = segment_score_cur_starting_at_0 + transition_score_cur_starting_at_0
+                        #transition_score_cur_starting_at_0 = self.transitions[label_cur_idx, label_cur_idx] * (segment_len - 1) 
+                        #transition_score_cur_starting_at_0 *= self.transition_importance
+                        #complete_score_cur_starting_at_0 = segment_score_cur_starting_at_0 + transition_score_cur_starting_at_0
+                        complete_score_cur_starting_at_0 = segment_score_cur_starting_at_0
                     
                     # Segments not starting at 0
                     # j + 1 is the length of the frame
-                    segment_score_cur_with_chord_change = torch.stack([self.segment_score[sample_idx, end_frame - max_span + 2 + j, end_frame, label_cur_idx] * (j + 1) for j in range(max_span - 1)], dim=0)
-                    transition_score_cur_with_chord_change = self.transitions[torch.tensor(range(self.num_label)), label_cur_idx] * self.transition_importance
-                    transition_score_cur_with_chord_change = torch.stack([transition_score_cur_with_chord_change + self.transitions[label_cur_idx, label_cur_idx] * j for j in range(max_span - 1)], dim=0)
-                    complete_score_cur_with_chord_change = segment_score_cur_with_chord_change[:, None] + transition_score_cur_with_chord_change
+                    segment_score_cur_with_harmony_change = torch.stack([self.segment_score[sample_idx, end_frame - max_span + 2 + j, end_frame, label_cur_idx] * (j + 1) for j in range(max_span - 1)], dim=0)
+                    #transition_score_cur_with_harmony_change = self.transitions[torch.tensor(range(self.num_label)), label_cur_idx] * self.transition_importance
+                    #transition_score_cur_with_harmony_change = torch.stack([transition_score_cur_with_harmony_change + self.transitions[label_cur_idx, label_cur_idx] * self.transition_importance * j for j in range(max_span - 1)], dim=0)
+                    #complete_score_cur_with_harmony_change = segment_score_cur_with_harmony_change[:, None] + transition_score_cur_with_harmony_change
+                    complete_score_cur_with_harmony_change = segment_score_cur_with_harmony_change[:, None]
                     
                     V_pre = V[end_frame - max_span + 1 : end_frame, :]
                     
-                    total_score_with_chord_change = V_pre + complete_score_cur_with_chord_change
+                    total_score_with_harmony_change = V_pre + complete_score_cur_with_harmony_change
                     
-                    pre_pos_with_chord_change = (total_score_with_chord_change==torch.max(total_score_with_chord_change)).nonzero()
-                    pre_pos_with_chord_change = pre_pos_with_chord_change[0, :]
-                    total_score_with_chord_change_max = total_score_with_chord_change[pre_pos_with_chord_change[0], pre_pos_with_chord_change[1]]
+                    pre_pos_with_harmony_change = (total_score_with_harmony_change==torch.max(total_score_with_harmony_change)).nonzero()
+                    pre_pos_with_harmony_change = pre_pos_with_harmony_change[0, :]
+                    total_score_with_harmony_change_max = total_score_with_harmony_change[pre_pos_with_harmony_change[0], pre_pos_with_harmony_change[1]]
                     
-                    if total_score_with_chord_change_max > complete_score_cur_starting_at_0:
-                        V[end_frame, label_cur_idx] = total_score_with_chord_change_max
-                        pre_pos_with_chord_change[0] += end_frame - max_span + 1
-                        pre_pos[end_frame, label_cur_idx] = pre_pos_with_chord_change
+                    if total_score_with_harmony_change_max > complete_score_cur_starting_at_0:
+                        V[end_frame, label_cur_idx] = total_score_with_harmony_change_max
+                        pre_pos_with_harmony_change[0] += end_frame - max_span + 1
+                        pre_pos[end_frame, label_cur_idx] = pre_pos_with_harmony_change
                     else:
                         V[end_frame, label_cur_idx] = complete_score_cur_starting_at_0
                         pre_pos[end_frame, label_cur_idx] = torch.tensor([-1, -1])
